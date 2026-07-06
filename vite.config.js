08f7358900197b24144b739ec95bfa7f99314d12
Atish Bansode <atishbansode2021@gmail.com>
2026-07-06 21:45:32 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,26 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'vendor-react',
+              test: /node_modules[\\/](react|react-dom|react-router-dom)/
+            },
+            {
+              name: 'vendor-framer',
+              test: /node_modules[\\/]framer-motion/
+            },
+            {
+              name: 'vendor-lucide',
+              test: /node_modules[\\/]lucide-react/
+            }
+          ]
+        }
+      }
+    }
+  }
 })
